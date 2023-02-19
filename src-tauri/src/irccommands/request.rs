6763aefdc4b573656e9bc97b3f5c52c7386b191c
tikliz <tik8lizz@gmail.com>
2023-02-19 10:23:03 -0300

    implemented html-json parsing for osu requests

--- src-tauri/src/irccommands/request.rs
+++ src-tauri/src/irccommands/request.rs
@@ -1,12 +1,53 @@
-use tauri::{window, Window, Manager};
+use std::fs;
+
+use futures::executor::block_on;
+use serde_json::Value;
+use tokio::{runtime::Runtime, task};
+use reqwest::{self, get};
+use tauri::{window, Window, Manager, regex};
 use url::Url;
+use scraper::{Html, Selector};
 use crate::bot;
+
+struct Payload {
+    map_bg: String,
+    map_title: String,
+    map_artist: String,
+    mapper: String,
+    requester: String,
+
+}
 
 fn is_valid_url(url_str: &str) -> bool {
     match Url::parse(url_str) {
         Ok(_) => true,
         Err(_) => false,
     }
+}
+
+async fn create_payload(url: String) {
+    println!("<parsing request!>");
+    let response = get(url).await.unwrap();
+
+    // parse the HTML response
+    let body = response.text().await.unwrap();
+    let document = Html::parse_document(&body);
+
+    // extract json
+    let selector = Selector::parse("#json-beatmapset").unwrap();
+    let script = document.select(&selector).next().unwrap().inner_html();
+    let json: Value = serde_json::from_str(&script).unwrap();
+    
+    println!("Artist: {}", json["artist"]);
+    println!("Title: {}", json["title"]);
+    println!("Creator: {}", json["creator"]);
+    println!("id: {}", json["id"]);
+    println!("NSFW: {}", json["nsfw"]);
+    println!("BPM: {}", json["bpm"]);
+    println!("noms?: {}", json["nominations_summary"]);
+    println!("bg: {}", json["covers"]["cover"]);
+    // println!("bg 2x: {}", json["covers"]["cover@2x"]);
+
 }
 
 pub fn run(bot: &mut bot::Irc, handler: &Vec<bot::CommandRegister>, target: &String, msg: Option<&String>, window: Option<&Window>) -> Option<String> {
@@ -22,6 +63,14 @@ pub fn run(bot: &mut bot::Irc, handler: &Vec<bot::CommandRegister>, target: &Str
             return Some("Invalid argument, this is not a valid URL  + help".to_string());
 
         }
+        if args[0].contains("https://osu.ppy.sh/beatmapsets/") {
+            task::spawn(create_payload(args[0].to_string()));
+
+        } else {
+            return Some("Not an osu! request, maybe valid.".to_string());
+
+        }
+        
         window.unwrap().emit_all("ADD_TO_LIST", args[0]).unwrap();
         return Some("Done!".to_string());
 
